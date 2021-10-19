08ee168f38e6e5b3244782de2e9d687a02fcb12e
Raphael <raphael@arcplex.fr>
2021-10-19 16:39:36 +0200

    correction

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,9 +2,45 @@ import React from "react";
 import "./Cart.css";
 import { useHistory } from "react-router-dom";
 import CartElement from "../CartElement/CartElement";
+import { KebabContext } from "../../Store/KebabContext";
 
 export default function Cart() {
+  const { kebabState, kebabDispatch } = React.useContext(KebabContext);
   let history = useHistory();
+  const [body, setBody] = React.useState();
+  const [getValues, setGetValues] = React.useState(true);
+
+  React.useEffect(() => {
+    const buildCart = () => {
+      setBody(
+        kebabState.kebabCart.map((value, key) => {
+          let sauces = "";
+          let garniture = "";
+          for (key in value.sauces) {
+            if (key > 0) {
+              sauces = sauces + " - " + value.sauces[key];
+            } else {
+              sauces = value.sauces[key];
+            }
+          }
+          for (key in value.garnitures) {
+            if (key > 0) {
+              garniture = garniture + " - " + value.garnitures[key];
+            } else {
+              garniture = value.garnitures[key];
+            }
+          }
+          const title =
+            value.pain + ", " + value.viande + ", " + garniture + ", " + sauces;
+          return <CartElement text={title} quantity={"1"}></CartElement>;
+        })
+      );
+    };
+    if (getValues) {
+      buildCart();
+      setGetValues(false);
+    }
+  }, [getValues]);
   return (
     <div className="cartContainer">
       <div className="containerPrix">
@@ -17,10 +53,8 @@ export default function Cart() {
         </button>
       </div>
       <div className="containerPannier">
-      <p className="price">Votre commande</p> <br />
-        <CartElement></CartElement>
-        <CartElement></CartElement>
-        <CartElement></CartElement>
+        <p className="price">Votre commande</p> <br />
+        {body}
       </div>
     </div>
   );
--- src/Components/CartElement/CartElement.jsx
+++ src/Components/CartElement/CartElement.jsx
@@ -3,8 +3,8 @@ import './CartElement.css';
 export default function CartElement(props) {
     return (
         <div className="elementContainer">
-            <p className="containerKebab">kebab viande complet, blanche, bbq</p>
-            <p>x1</p>
+            <p className="containerKebab">{props.text}</p>
+            <p>{props.quantity}</p>
         </div>
     )
 }
