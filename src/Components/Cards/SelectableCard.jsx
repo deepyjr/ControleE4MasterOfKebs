525beec2d47abf93ad18eda857b5828f41341d61
Raphael <raphael@arcplex.fr>
2021-10-19 12:00:27 +0200

    update fonctionnel + graphique cards

--- src/Components/Cards/SelectableCard.jsx
+++ src/Components/Cards/SelectableCard.jsx
@@ -10,7 +10,7 @@ export default function SelectableCard(props) {
     return (
         <div className="selectable-card-container" onClick={handleClick}>
             {selected ? <img src={process.env.PUBLIC_URL + "/Check.png"} className="checkImage"></img> : <div className="divVide"></div>}
-            <img src={process.env.PUBLIC_URL + "/Kebab.png"} className="cardImage"></img>
+            <img src={process.env.PUBLIC_URL + "/"+props.image+".png"} className="cardImage"></img>
             <h6>{props.title}</h6>
         </div>
     )
--- src/Views/1SelectionPain.jsx
+++ src/Views/1SelectionPain.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import './Process.css'
+import React from "react";
+import "./Process.css";
 import SelectableCard from "../Components/Cards/SelectableCard";
 
 export default function SelectionPain() {
-    return (
-        <div>
-            <h1 className="Title">Selection du pain</h1>
-            <SelectableCard title="Pain"></SelectableCard>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="Title">Selection du pain</h1>
+      <div className="containerCards">
+        <SelectableCard title="Pain" image="Pain"></SelectableCard>
+        <SelectableCard title="Galette" image="Galette"></SelectableCard>
+      </div>
+    </div>
+  );
 }
